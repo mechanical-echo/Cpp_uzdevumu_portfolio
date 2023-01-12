6d7835be5f1c2fc7a085df6cf8be3544d66abbc4
Sofia Dyshlovaya <105802387+mechanical-echo@users.noreply.github.com>
2023-01-12 06:33:55 +0000

    Creaeted first variables, their input and  array creation. Added commentaries

--- .ccls-cache/@home@runner@Cpp120123/main.cpp
+++ .ccls-cache/@home@runner@Cpp120123/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-
+#include <stdio.h>
 int main() {
-  std::cout << "Hello World!\n";
+  int n, m ,k;
+  //collumns, lines, mine count
+  
+  std::cout<<"Ievadi n";
+  std::cin>>n; //lines
+  
+  std::cout<<"Ievadi m";
+  std::cin>>m; //collumns
+  
+  std::cout<<"Ievadi k";
+  std::cin>>k; //mine count
+  
+  char laukums[n][m]; //game field
+  
+  srand(NULL); //this line makes random numbers to not repeat after previous run
+
+  //spreading mines randomly
+  for(int i=0; i<k; i++){
+    int t = 1+(rand()%9);
+    
+  }
 }
