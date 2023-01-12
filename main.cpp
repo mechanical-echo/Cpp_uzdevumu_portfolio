6106088b355ed89c8d406fc119aa2c9dd7348872
Sofia Dyshlovaya <105802387+mechanical-echo@users.noreply.github.com>
2023-01-12 08:46:52 +0000

    Completed working on a array creation; Fixed error;

--- main.cpp
+++ main.cpp
@@ -1,41 +1,124 @@
 #include <iostream>
-#include <stdio.h>
+using namespace std;
 int main() {
-  int n, m ,k;
-  //collumns, lines, mine count
+  int n, m, k;
+  // collumns, lines, mine count
   
-  std::cout<<"Ievadi n";
-  std::cin>>n; //lines
-  
-  std::cout<<"Ievadi m";
-  std::cin>>m; //collumns
   do{
-    std::cout<<"Ievadi k";
-    std::cin>>k; //mine count
-    if(k>=(n*m)){
-      std::cout<<"Mīnu ir padaudz!";
-    }
-  }while(k>=(n*m));
-  
-  char laukums[n][m]; //game field
-  
-  srand(NULL); //this line makes random numbers to not repeat after previous run
+    cin >> n; // lines
+  }while(n<1);
 
-  //spreading mines randomly
-  int t1, t2;
-  bool check = false;
-  for(int i=0; i<k; i++){
-    do{
-      t1 = rand()%(n-1);  //random coordinates of mine
-      t2 = rand()%(m-1);
-      
-      if(laukums[t1][t2]!='*'){ //checks if this place already has the mine
-        check = false; //if there is no mine, then programm places it and it does not search for a new place for this exact mine
-        laukums[n][m] = '*';
-        }else{
-        check = true; //if there is mine already, check becomes true and do-while cycle begins again to find another place for this mine 
+  do{
+    cin >> m; // collumns
+  }while(m>1000);
+
+  do{
+    cin >> k; // mine count
+  }while(k>100000 || k>(n*m));  
+
+  char laukums[n][m]; // game field
+
+  srand(time(NULL));
+
+  // spreading mines randomly
+  int counter = k;
+  do {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        int test = rand() % 2;
+        if (laukums[i][j] != '*' && counter != 0 && test == 1) {
+          laukums[i][j] = '*';
+          counter--;
+        }
       }
-    }while(check);
-    
+    }
+  } while (counter > 0);
+
+  int nearest_count = 0;
+
+  for (int a = 0; a < n; a++) { // a b coordinates of place that isnt a mine
+    for (int b = 0; b < m; b++) {
+      nearest_count = 0;
+      if (laukums[a][b] != '*') {
+        /* Next code checks every nearest tile and adds 1 every time if it finds
+           mine
+           TODO :optimize code and make it shorter
+        */
+
+        if ((b - 1) >= 0) {
+          if (a - 1 >= 0) {
+            if (laukums[a - 1][b - 1] == '*') {
+              nearest_count++;
+            }
+          }
+          if (laukums[a][b - 1] == '*') {
+            nearest_count++;
+          }
+          if (a + 1 < n) {
+            if (laukums[a + 1][b - 1] == '*') {
+              nearest_count++;
+            }
+          }
+        } /////////
+
+        if (a - 1 >= 0) {
+          if (laukums[a - 1][b] == '*') {
+            nearest_count++;
+          }
+          if ((b + 1) < m) {
+            if (laukums[a - 1][b + 1] == '*') {
+              nearest_count++;
+            }
+          }
+        } /////
+
+        if (b + 1 < m) {
+          if (laukums[a][b + 1] == '*') {
+            nearest_count++;
+          }
+        }
+
+        if (a + 1 < n) {
+          if (laukums[a + 1][b] == '*') {
+            nearest_count++;
+          }
+        }
+
+        if (a + 1 < n && b + 1 < m) {
+          if (laukums[a + 1][b + 1] == '*') {
+            nearest_count++;
+          }
+        }
+
+        // After all the checking - programm gives ab tile a number of nearest
+        // mine count
+       // cout << nearest_count << "\n\n";
+        std::string s = std::to_string(nearest_count);
+        char const *pchar = s.c_str();
+        laukums[a][b] = *pchar;
+        //laukums[a][b] = nearest_count;
+      } // if
+    }   // int b
+  }     // int a
+
+  // TODO : delete next later
+  // just to check array
+  /*
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << laukums[i][j] << "\t";
+      // cout<<"\t";
+    }
+    cout << "\n";
   }
+  */
+  char minesweeper[n][m];
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      minesweeper[i][j] = '*';
+    }
+  }
+  
+  
+  
 }
