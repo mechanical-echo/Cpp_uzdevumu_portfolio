381b84b98a8804b4a13e38b8bfc8075810ea5ed8
Sofia Dyshlovaya <105802387+mechanical-echo@users.noreply.github.com>
2023-01-26 07:30:55 +0000

    Completed, probably

--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 int main() {
   int n, m, c;
-  cin>>n;
-  cin>>m;
-  cin>>c;
+  do{
+    cin>>n;
+  }while(1>=n);
+  do{
+    cin>>m;
+  }while(m>=10);
+  do{
+    cin>>c;
+  }while(1>=c || c>=105);
+    
   int field[n][m+1];
   int counter = 1;
-  /*
-0%2=0| 00  01  02 |03|
-1%2=1| 10  11  12  13
-2%2=0| 20  21  22 |23|
-    */
   for(int i=0; i<n; i++){
     for(int j=0; j<m+1; j++){
       if(i%2==0 && j==m){
@@ -22,12 +24,104 @@ int main() {
       }
     }
   }
-
-  for(auto& x : field){
-    for(auto& y : x){
-      cout<<y<<" ";
+  /*
+1 2 3 0
+4 5 6 7
+8 9 10 0 
+00  01  |02|  |03|
+10  11  |12|   13
+20  21  |22|  |23|
+i-1j-1  i-1j  i-1j+1
+ij-1     ij   ij+1 
+i+1j-1  i+1j  i+1j+1   
+*//*
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m+1; j++){
+      cout<<field[i][j]<<"  ";
     }
     cout<<"\n";
-  }
+  }*/
+
+  int neighbours[6];
+  counter=0;
+
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m+1; j++){
+      if(field[i][j]==c){
+        if(j==0){
+          if(i>0){
+
+            neighbours[counter] = field[i-1][j];
+            counter++;
+          }
+
+          neighbours[counter] = field[i][j+1];
+          counter++;
+          if(i+1<n){
+
+            neighbours[counter] = field[i+1][j];
+            counter++;
+          }
+        }else{
+            
+          if(i>0){
+
+            neighbours[counter] = field[i-1][j-1];
+            counter++;
+            neighbours[counter] = field[i-1][j];
+
+            counter++;
+          }
+            
+          if(j!=0){
+
+            neighbours[counter] = field[i][j-1];
+            counter++;
+            if(i+1<n){
   
+              neighbours[counter] = field[i+1][j-1];
+              counter++;
+              }
+          }
+          if(i+1<n){
+ 
+            neighbours[counter] = field[i+1][j];
+            counter++;
+          }
+          if(j+1<m+1){
+           
+            neighbours[counter] =  field[i][j+1];
+            counter++;
+          }
+          
+        }
+      }
+      
+    }
+  }
+  cout<<counter<<"\n";
+  if(counter<6){
+    for(int i=counter; i<6; i++){
+      neighbours[i] = 0;
+    }
+  }
+
+  /*
+1  2  3  4  5  0
+6  7  8  9  10 11
+12 13 14 15 16 0
+*/
+  for(int i=0; i<6; i++){
+    for(int j=i+1; j<6; j++){
+      if(neighbours[i]>neighbours[j]){
+        int temp = neighbours[i];
+        neighbours[i] = neighbours[j];
+        neighbours[j] = temp;
+      }
+    }
+  }
+  for(int i=0; i<6; i++){
+    if(neighbours[i]!=0)
+      cout<<neighbours[i]<<" ";
+  }
 }
