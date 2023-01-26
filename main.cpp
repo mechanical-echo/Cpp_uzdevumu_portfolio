f947b0cf09c1338d40bdf60e5e9dd43149a9ab5b
Sofia Dyshlovaya <105802387+mechanical-echo@users.noreply.github.com>
2023-01-26 06:14:45 +0000

    Created array that represents hexagon field with numbers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  int n, m, c;
+  cin>>n;
+  cin>>m;
+  cin>>c;
+  int field[n][m+1];
+  int counter = 1;
+  /*
+0%2=0| 00  01  02 |03|
+1%2=1| 10  11  12  13
+2%2=0| 20  21  22 |23|
+    */
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m+1; j++){
+      if(i%2==0 && j==m){
+        field[i][j]=0;
+      }else{
+        field[i][j] = counter; 
+        counter++;
+      }
+    }
+  }
+
+  for(auto& x : field){
+    for(auto& y : x){
+      cout<<y<<" ";
+    }
+    cout<<"\n";
+  }
+  
 }
