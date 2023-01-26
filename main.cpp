29477d7fcced647cb05cfb7846b3471e1281c208
Sofia Dyshlovaya <105802387+mechanical-echo@users.noreply.github.com>
2023-01-26 08:33:28 +0000

    first try, not done yet

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int n; //veidi
+  int m; //konfeksu skaits no katra veida glitos iepakojumos
+  do{
+    cin>>n;
+  }while(n<1);
+  do{
+    cin>>m;
+  }while(m>250000);
+  int a; //sarazotajs daudzums
+  int konfektes[n]; // katraa lodzinaa buus katrs veids
+  int iepakojumu_skaits[n];
+  int palikusi=0;
+  bool sanak= true;
+  for(int i=0; i<n; i++){
+    cin>>konfektes[i];
+    palikusi += konfektes[i]%m;
+    iepakojumu_skaits[i] = konfektes[i]/m;
+    if(i!=0){
+      if(iepakojumu_skaits[i]==iepakojumu_skaits[i-1] && sanak==true){
+        a = min(iepakojumu_skaits[i], iepakojumu_skaits[i-1]);
+        sanak=true;
+      }
+      else
+        sanak=false;
+    }
+  }
+
+  
+  cout<<a<<" "<<palikusi;
+  
+  
 }
