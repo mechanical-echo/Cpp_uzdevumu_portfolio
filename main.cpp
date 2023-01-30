43ffc5d9b3b23923994a2eea0f2b1d5ba42608eb
mechanical-echo <sofija45642@gmail.com>
2023-01-30 19:13:47 +0200

    passed 6/12

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
 int main() {
   int N;
   do{
     cin>>N;
   }while(N<1 || N>pow(10,9));
-  
-  
-  
+
+  int counter =0;
+  for(int i=N-1; i>=1; i--){
+    counter++;
+    if(N % i==0)
+    break;
+  }
+  cout<<counter;  
 }
